659372e1a02068cd521439951b892c86f69eeb17
zig <zig@kavanga.ru>
2013-09-13 14:02:09 +0400

    fixed

--- langs/de.js
+++ langs/de.js
@@ -1,61 +1,60 @@
-﻿if (typeof langs == "undefined") {
+﻿if ( typeof langs == "undefined" ) {
     var langs = {};
 }
 
 langs.de = {
-    'ConfirmDowngrade' : "Möchten Sie wirklich dieses Gebäude um 1 Stufe downgraden?",
-    'Circular_message' : "Mitteilung an alle",
-	'Ally_members'     : "Allianz",
-	'Attack_All'	   : "Alle",
-	'Attack_Half'	   : "Hälfte",
-	'Attack_Nothing'   : "Nichts",
-	'Required' 		   : "Nötig:",
-	'Next_Level'	   : "Nächste Stufe:",
-	'Messages'	   	   : "Nachrichten",
-	'Agora'	   		   : "Agora",
-	'Treaty'	       : "Abkommen",
-	'Alliance'	   	   : "Forum",
-	'Transport'		   : "Transportieren",
-	'Send_General_Log' : "Log an Allianz schicken",
-	'Send_General_Log_Header' : "Angriffe auf Allianzmitglieder",
-	
-	'options' : {
-		'transport' : {
-			'header' : 'Art des Ressourcentransports',
-			'original' : 'GameForge',
-			'Ikaeasy' : 'IkaEasy'
-		}
-	},
+    'ConfirmDowngrade': "Möchten Sie wirklich dieses Gebäude um 1 Stufe downgraden?",
+    'Circular_message': "Mitteilung an alle",
+    'Ally_members': "Allianz",
+    'Attack_All': "Alle",
+    'Attack_Half': "Hälfte",
+    'Attack_Nothing': "Nichts",
+    'Required': "Nötig:",
+    'Next_Level': "Nächste Stufe:",
+    'Messages': "Nachrichten",
+    'Agora': "Agora",
+    'Treaty': "Abkommen",
+    'Alliance': "Forum",
+    'Transport': "Transportieren",
+    'Send_General_Log': "Log an Allianz schicken",
+    'Send_General_Log_Header': "Angriffe auf Allianzmitglieder",
 
-    'ikalogs' : {
-        'save_log'  : 'Bericht speichern',
-        'analyze'   : 'Analysieren',
-        'each'      : 'jedes',
-        'help_bw'   : 'Geben sie die bestimmten Runden mit Kommas oder den gewünschten Rundenbereich mit Minuszeichen an.',
-
-        'get_info'  : 'Information bekommen',
-        'get_round' : 'Bericht bekommen',
-        'saving'    : 'Bericht speicher',
-
-        'saving_success' : 'Der Bericht wurde gespeichert.',
-        'open_report'    : 'Bericht öffnen',
-
-        'saving_failed'  : 'Der Bericht wurde nicht gespeichert',
-        'repeat'    : 'Versuch wiederholen',
-
-        'auth' : {
-            'not_logged' : 'Sie sind in ikalogs nicht autorisiert'
-        },
-
-
-        'types'     : {
-            'full'  : 'Vollbericht',
-            'short' : 'Kurzbericht',
-            'last'  : 'Letzte Runde',
-            'each'  : 'Jede N Runde',
-            'between'  : 'Bestimmte Runden'
+    'options': {
+        'transport': {
+            'header': 'Art des Ressourcentransports',
+            'original': 'GameForge',
+            'Ikaeasy': 'IkaEasy'
         }
     },
 
-    'Transporter' : 'Transporter'
+    'ikalogs': {
+        'save_log'  : 'Kampfberichte speichern',
+        'analyze'   : 'speichern',
+        'each'      : 'jede',
+        'help_bw'   : 'Durch Komma-separierte Liste, oder durch einen Bindestrich eine Reihe von Runden.',
+
+        'get_info'  : 'sammle Daten',
+        'get_round' : 'KB Daten',
+        'saving'    : 'speichere KB',
+
+        'saving_success': 'Der KB wurde gespeichert.',
+        'open_report'   : 'öffne gespeicherten KB',
+
+        'saving_failed' : 'Fehler beim speichern des KB',
+        'repeat'        : 'versuche es erneut',
+
+        'auth': {
+            'not_logged': 'Du bist nicht eingelogt in Ikalogs'
+        },
+
+        'types': {
+            'full'    : 'gesamten KB',
+            'short'   : 'kurzer KB',
+            'last'    : 'letzte Runde',
+            'each'    : 'jede N Runde',
+            'between' : 'Runde wählen'
+        }
+    },
+
+    'Transporter': 'Transporter'
 };
