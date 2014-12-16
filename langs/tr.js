ef11b458c4d3e1b8b18bac9bed7661f3e8059f5d
Akın Ayturan <akinayturan@gmail.com>
2014-12-16 08:57:19 +0200

    Create tr.js
    Translating from English to Turkish, it will improve when i see on the game :)

---
+++ langs/tr.js
@@ -0,0 +1,86 @@
+if(typeof langs == "undefined") {
+    var langs = {};
+}
+
+langs.en = {
+    'movements':{
+        all: 'hepsi',
+        transport: 'transport',
+        plunder: 'plunder',
+        blockade: 'blockade',
+        deployarmy: 'birimleri terhis et',
+        deployfleet: 'deploy fleet',
+        defend: 'savun',
+        defend_port: 'limanı savun',
+        occupy: 'işgal',
+        trade: 'ticaret'
+    },
+    'char_day': 'd',
+    'char_hour': 'h',
+    'char_minute': 'm',
+    'char_second': 's',
+    'WillBeFullIn': 'Be filled in',
+    'per_day': "Her gün",
+    'per_week': "Her hafta",
+    'per_month': "Her ay",
+    'left': "Sol",
+    'hours': "saat",
+    'days': "gün",
+    'months': "ay",
+    'years': "yıl",
+    'MessagePlaceholder': ""CTRL"ye tıkla + mesajı göndermek için "Enter"a tıkla",
+    'ConfirmDowngrade': "Binayı yıkmak için onayla",
+    'Circular_message': "Herkese mesaj gönder",
+    'Ally_members': "ittifak",
+    'Attack_All': "All",
+    'Attack_Half': "Half",
+    'Attack_Nothing': "Nothing",
+    'Required': "Required:",
+    'Next_Level': "Sonraki Seviye: ",
+    'Messages': "Mesajlar",
+    'Agora': "Agora",
+    'Treaty': "Antlaşma",
+    'Alliance': "Board",
+    'Transport': "Transport goods!",
+    'Send_General_Log': "Send log to ally",
+    'Send_General_Log_Header': "Attacks on alliance members",
+
+    'options': {
+        'transport': {
+            'header': 'Transporting options type',
+            'original': 'GameForge',
+            'ikaeasy': 'IkaEasy'
+        }
+    },
+
+    'ikalogs': {
+        'save_log': 'Save log',
+        'analyze': 'Analyze',
+        'each': 'Every',
+        'help_bw': 'Seperate individual rounds with a comma, or a range of rounds with a dash.',
+
+        'get_info': 'Getting data',
+        'get_round': 'Getting log',
+        'saving': 'Saving log',
+
+        'saving_success': 'The log was successfully saved.',
+        'open_report': 'Open log',
+
+        'saving_failed': 'Failed to save log',
+        'repeat': 'Tekrar deneyin',
+
+        'auth': {
+            'not_logged': 'You are not logged on Ikalogs'
+        },
+
+        'types': {
+            'full': 'Full log',
+            'short': 'Short log',
+            'last': 'Last round',
+            'each': 'Every N round',
+            'between': 'Specific rounds'
+        }
+    },
+
+    'Transporter': 'Transporter'
+};
