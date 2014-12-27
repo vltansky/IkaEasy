645be15a2023d36c6b7da3722e633de97188fc52
Vlad Tansky <vl.tansky@gmail.com>
2014-12-27 03:07:31 +0200

    polish lang

---
+++ langs/pl.js
@@ -0,0 +1,87 @@
+if(typeof langs == "undefined") {
+    var langs = {};
+}
+
+langs.en = {
+    'ikaeasy_link': 'https://www.facebook.com/ikariam.easy',
+    'movements':{
+        all: 'all',
+        transport: 'transport',
+        plunder: 'plądrowanie',
+        blockade: 'blokada',
+        deployarmy: 'rozmieść wojsko',
+        deployfleet: 'rozmieść flotę',
+        defend: 'broń',
+        defend_port: 'broń portu',
+        occupy: 'okupacja',
+        trade: 'handel'
+    },
+    'char_day': 'd',
+    'char_hour': 'h',
+    'char_minute': 'm',
+    'char_second': 's',
+    'WillBeFullIn': 'będzie pełne w ciągu',
+    'per_day': "na dzień",
+    'per_week': "Na tydzień",
+    'per_month': "Na miesiąc",
+    'left': "zostało",
+    'hours': "godziny",
+    'days': "dni",
+    'months': "miesiące",
+    'years': "lata",
+    'MessagePlaceholder': "Wciśnij CTRL + Enter, aby wysłać wiadomość",
+    'ConfirmDowngrade': "Potwierdź wyburzenie budynku",
+    'Circular_message': "Wiadomość do wszystkich",
+    'Ally_members': "Sojusz",
+    'Attack_All': "Wszystko",
+    'Attack_Half': "Połowa",
+    'Attack_Nothing': "Nic",
+    'Required': "Wymagane:",
+    'Next_Level': "Następny poziom:",
+    'Messages': "Wiadomości",
+    'Agora': "Agora",
+    'Treaty': "Traktat",
+    'Alliance': "Tablica",
+    'Transport': "Transportuj dobra!",
+    'Send_General_Log': "Wyślij logi sojusznikom",
+    'Send_General_Log_Header': "Ataki na członków sojuszu",
+
+    'options': {
+        'transport': {
+            'header': 'Transporting options type',
+            'original': 'GameForge',
+            'ikaeasy': 'IkaEasy'
+        }
+    },
+
+    'ikalogs': {
+        'save_log': 'Zapisz log',
+        'analyze': 'Analiza',
+        'each': 'Każde',
+        'help_bw': 'Oddziel kolejne rundy kropkami lub serię rund apostrofami.',
+
+        'get_info': 'Pozyskiwanie danych',
+        'get_round': 'Tworzenie logu',
+        'saving': 'Zapisywanie logu',
+
+        'saving_success': 'Ten log został pomyślnie zapisany.',
+        'open_report': 'Otwórz log',
+
+        'saving_failed': 'Nie udało się zapisać logu.',
+        'repeat': 'Spróbuj ponownie',
+
+        'auth': {
+            'not_logged': 'Nie jesteś zalogowany na Ikalogs'
+        },
+
+        'types': {
+            'full': 'Pełny log',
+            'short': 'Skrócony log',
+            'last': 'Ostatnia runka',
+            'each': 'co 5 runda',
+            'between': 'wybrane rundy'
+        }
+    },
+
+    'Transporter': 'Transporter'
+};
