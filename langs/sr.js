2bd6d9b18a5fd002b141a9574f95ace306edc719
Akın Ayturan <akinayturan@gmail.com>
2014-12-30 00:00:30 +0200

    Update and rename sr.txt to sr.js

---
+++ langs/sr.js
@@ -0,0 +1,90 @@
+if (typeof langs == "undefined") {
+    var langs = {};
+}
+
+//Serbian language
+langs.sr = {
+    'ikaeasy_link': 'https://www.facebook.com/ikariam.easy',
+    'movements':{
+        all: 'all',
+        transport: 'transport',
+        plunder: 'plunder',
+        blockade: 'blockade',
+        deployarmy: 'deploy army',
+        deployfleet: 'deploy fleet',
+        defend: 'defend',
+        defend_port: 'defend port',
+        occupy: 'occupy',
+        trade: 'trade'
+    },
+    'char_day': 'd',
+    'char_hour': 'h',
+    'char_minute': 'm',
+    'char_second': 's',
+    'WillBeFullIn': 'Be filled in',
+    'per_day': "Per day",
+    'per_week': "Per week",
+    'per_month': "Per month",
+    'left': "Left",
+    'hours': "hours",
+    'days': "days",
+    'months': "months",
+    'years': "years",
+    'MessagePlaceholder': "Click CTRL + Enter to send message",
+    'ConfirmDowngrade' : "Да ли стварно желите да срушите 1 ниво ове зграде?",
+    'Circular_message' : "Порука свима",
+    'Ally_members'     : "Савез",
+    'Attack_All'	   : "Све",
+    'Attack_Half'	   : "Половина",
+    'Attack_Nothing'   : "Ништа",
+    'Required' 		   : "Потребно је:",
+    'Next_Level'	   : "Следећи ниво:",
+    'Messages'	   	   : "Поруке",
+    'Agora'	   		   : "Агора",
+    'Treaty'	       : "Споразум",
+    'Alliance'	   	   : "Форум",
+    'Transport'		   : "Транспортирати!",
+    'Send_General_Log' : "Послати извештај савезу",
+    'Send_General_Log_Header' : "Напади на чланове савеза",
+
+
+    'options': {
+        'transport': {
+            'header': 'Врста транспорта ресурса',
+            'original': 'GameForge',
+            'ikaeasy': 'IkaEasy'
+        }
+    },
+    
+    'ikalogs' : {
+        'save_log'  : 'Сачувати извештај',
+        'analyze'   : 'Анализирати',
+        'each'      : 'Свака',
+        'help_bw'   : 'Упишите појединачне рунде са зарезом, или опсег рунда са цртицом.,
+
+        'get_info'  : 'Добијање информације',
+        'get_round' : 'Добијање извештаја',
+        'saving'    : 'Сачување извештаја',
+
+        'saving_success' : 'Извештај је успешно сачуван.',
+        'open_report'    : 'Отворити извештај',
+
+        'saving_failed'  : 'Извештај није сачуван',
+        'repeat'    : 'Покушати још једном',
+
+        'auth' : {
+            'not_logged' : 'Ви нисте ауторизирани на ikalogs'
+        },
+
+
+        'types'     : {
+            'full'  : 'Пун извештај',
+            'short' : 'Кратак извештај',
+            'last'  : 'Последња рунда',
+            'each'  : 'Сваку N рунду',
+            'between'  : 'Одређене рунде'
+        }
+    },
+
+    'Transporter' : 'Транспортер'
+};
