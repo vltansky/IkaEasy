cc52ab5740f70a692506d9797d299e9e674e0acb
Vlad Tansky <vl.tansky@gmail.com>
2015-01-05 09:35:10 +0200

    greek translation

---
+++ langs/el.js
@@ -0,0 +1,88 @@
+if(typeof langs == "undefined") {
+    var langs = {};
+}
+
+//Greek language
+langs.gr = {
+    'ikaeasy_link': 'https://www.facebook.com/ikariam.easy',
+    'movements':{
+        all: 'όλα',
+        transport: 'μεταφορά',
+        plunder: 'λεηλασία',
+        blockade: 'αποκλεισμός',
+        deployarmy: 'παράταξε τον στρατό',
+        deployfleet: 'παράταξε τον στόλο',
+        defend: 'υπεράσπιση',
+        defend_port: 'υπεράσπιση λιμανιού',
+        occupy: 'κατάληψη',
+        trade: 'εμπόριο'
+    },
+    'char_day': 'η',
+    'char_hour': 'ω',
+    'char_minute': 'λ',
+    'char_second': 'δ',
+    'WillBeFullIn': 'Θα γεμίσει σε',
+    'per_day': "ανά ημέρα",
+    'per_week': "ανά εβδομάδα",
+    'per_month': "ανά μήνα",
+    'left': "απομένουν",
+    'hours': "ώρες",
+    'days': "ημέρες",
+    'months': "μήνες",
+    'years': "χρόνια",
+    'MessagePlaceholder': "Κάντε κλικ στο κουμπί CTRL + ENTER για να στείλετε το μήνυμα",
+    'ConfirmDowngrade': "Επιβεβαιώστε την υποβάθμιση του κτιρίου",
+    'Circular_message': "Μήνυμα προς όλους",
+    'Ally_members': "Συμμάχια",
+    'Attack_All': "όλα",
+    'Attack_Half': "τα μισά",
+    'Attack_Nothing': "τίποτα",
+    'Required': "Απαιτείται:",
+    'Next_Level': "Επόμενο επίπεδο:",
+    'Messages': "Μηνύματα",
+    'Agora': "Αγορά",
+    'Treaty': "Σύνθηκη",
+    'Alliance': "Φόρουμ",
+    'Transport': "Μεταφορές εμπορευμάτων!",
+    'Send_General_Log': "Αποστολή αρχείου καταγραφής σε σύμμαχο",
+    'Send_General_Log_Header': "Επιθέσεις σε μέλη της συμμαχίας",
+
+    'options': {
+        'transport': {
+            'header': 'Επιλογή τύπου μεταφοράς',
+            'original': 'GameForge',
+            'ikaeasy': 'IkaEasy'
+        }
+    },
+
+    'ikalogs': {
+        'save_log': 'Αποθήκευση αρχείου καταγραφής',
+        'analyze': 'Ανάλυση',
+        'each': 'κάθε',
+        'help_bw': 'Διαχωρίστε τις μεμονωμένες γύρους με ένα κόμμα, ή μια σειρά από γύρους με μια παύλα.',
+
+        'get_info': 'Λήψη δεδομένων',
+        'get_round': 'Λήψη αρχείου καταγραφής',
+        'saving': 'Αποθήκευση αρχείου καταγραφής',
+
+        'saving_success': 'Το αρχείο καταγραφής με επιτυχία σωθεί.',
+        'open_report': 'Άνοιγμα αρχείου καταγραφής',
+
+        'saving_failed': 'Αποτυχία να αποθήκευσεις αρχείου',
+        'repeat': 'Προσπάθησε ξανά',
+
+        'auth': {
+            'not_logged': 'Δεν έχετε συνδεθεί στο Ikalogs'
+        },
+
+        'types': {
+            'full': 'Πλήρης αρχείο',
+            'short': 'Σύντομο αρχείο',
+            'last': 'Τελευταίος γύρος',
+            'each': 'Κάθε Ν γύρος',
+            'between': 'Συγκεκριμένους γύρους'
+        }
+    },
+
+    'Transporter': 'Μεταφορέας'
+};
