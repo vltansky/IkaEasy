6633a9c8eaf94964cd22871b505d32b40cf2028e
Vlad Tansky <vl.tansky@gmail.com>
2015-02-23 16:38:15 +0200

    german translation

--- langs/de.js
+++ langs/de.js
@@ -7,37 +7,37 @@ langs.de = {
     'ikaeasy_link': 'https://www.facebook.com/ikariam.easy',
     'movements':{
         all: 'alle',
-        transport: 'verkehr',
+        transport: 'transportieren',
         plunder: 'plündern',
-        blockade: 'vlockade',
-        deployarmy: 'vereitstellung Armee',
-        deployfleet: 'Bereitstellung Flotte',
-        defend: 'verteidigen',
-        defend_port: 'verteidigen Hafen',
+        blockade: 'blockieren',
+        deployarmy: 'Armee stationieren',
+        deployfleet: 'Flotte stationieren',
+        defend: 'Stadt verteidigen',
+        defend_port: 'Hafen verteidigen',
         occupy: 'besetzen',
-        trade: 'trade'
+        trade: 'handeln'
     },
     'char_day': 'd',
     'char_hour': 'h',
     'char_minute': 'm',
     'char_second': 's',
-    'WillBeFullIn': 'wird durch ausgefüllt werden',
+    'WillBeFullIn': 'Lagerhaus voll in', //warehouse full in
     'per_day': "Pro Tag",
-    'per_week': "pro Woche",
+    'per_week': "Pro Woche",
     'per_month': "Pro Monat",
-    'left': "links",
-    'hours': "Stunden.",
-    'days': "Tage.",
-    'months': "Monate",
-    'years': "Jahre",
-    'MessagePlaceholder': "Klik op CTRL + Enter om bericht te sturen",//not sure
-    'ConfirmDowngrade': "Möchten Sie wirklich dieses Gebäude um 1 Stufe downgraden?",
+    'left': "Übrig",
+    'hours': "Stunde(n)",
+    'days': "Tag(e)",
+    'months': "Monat(e)",
+    'years': "Jahr(e)",
+    'MessagePlaceholder': "Drücke STRG + Enter um die Nachricht zu verschicken.",//not sure
+    'ConfirmDowngrade': "Möchten Sie wirklich dieses Gebäude um eine Stufe verringern?",
     'Circular_message': "Mitteilung an alle",
     'Ally_members': "Allianz",
     'Attack_All': "Alle",
     'Attack_Half': "Hälfte",
     'Attack_Nothing': "Nichts",
-    'Required': "Nötig:",
+    'Required': "Notwendig:",
     'Next_Level': "Nächste Stufe:",
     'Messages': "Nachrichten",
     'Agora': "Agora",
@@ -57,19 +57,20 @@ langs.de = {
 
     'ikalogs': {
         'save_log': 'Kampfberichte speichern',
-        'analyze': 'speichern',
+        'analyze': 'Analysieren',
         'each': 'jede',
-        'help_bw': 'Durch Komma-separierte Liste, oder durch einen Bindestrich eine Reihe von Runden.',
+        'help_bw': 'Trenne jede Runde mit Kommas oder mehrere Runden mit einem Strich.',
 
-        'get_info': 'sammle Daten',
+
+        'get_info': 'Sammle Daten',
         'get_round': 'KB Daten',
-        'saving': 'speichere KB',
+        'saving': 'Speichere KB',
 
         'saving_success': 'Der KB wurde gespeichert.',
-        'open_report': 'öffne gespeicherten KB',
+        'open_report': 'Öffne gespeicherten KB',
 
-        'saving_failed': 'Fehler beim speichern des KB',
-        'repeat': 'versuche es erneut',
+        'saving_failed': 'Fehler beim Speichern des KB',
+        'repeat': 'Versuche es erneut',
 
         'auth': {
             'not_logged': 'Du bist nicht eingelogt in Ikalogs'
@@ -79,7 +80,7 @@ langs.de = {
             'full': 'gesamten KB',
             'short': 'kurzer KB',
             'last': 'letzte Runde',
-            'each': 'jede N Runde',
+            'each': 'jede N Runden',
             'between': 'Runde wählen'
         }
     },
