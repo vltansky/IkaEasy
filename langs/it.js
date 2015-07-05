d46202848785a693c76d6abcc93d46046d435cd4
Tansky <vl.tansky@gmail.com>
2015-07-05 21:16:46 +0300

    Italian translation by Luca Maglie

---
+++ langs/it.js
@@ -0,0 +1,106 @@
+if(typeof langs == "undefined") {
+    var langs = {};
+}
+
+//Italian language
+langs.it = {
+    'ikaeasy_link': 'https://www.facebook.com/ikariam.easy',
+    'movements':{
+        all: 'tutti',
+        transport: 'trasporto',
+        plunder: 'bottino',
+        blockade: 'blocco',
+        deployarmy: 'schiera esercito',
+        deployfleet: 'schiera flotta',
+        defend: 'difese',
+        defend_port: 'difese porto',
+        occupy: 'occupare',
+        trade: 'commercio'
+    },
+    'char_day': 'd',
+    'char_hour': 'h',
+    'char_minute': 'm',
+    'char_second': 's',
+    'WillBeFullIn': 'Completo in',
+    'per_day': "Ogni giorno",
+    'per_week': "Ogni settimana",
+    'per_month': "Ogni mese",
+    'left': "Manca",
+    'hours': "ore",
+    'days': "giorni",
+    'months': "mesi",
+    'years': "anni",
+    'MessagePlaceholder': "Clicca CTRL + INVIO per inviare il messaggio",
+    'ConfirmDowngrade': "Confermare il declassamento della costruzione",
+    'Circular_message': "Messaggio per tutti",
+    'Ally_members': "Alleanza",
+    'Attack_All': "Tutti",
+    'Attack_Half': "Metà",
+    'Attack_Nothing': "Nessuno",
+    'Required': "Richiesto:",
+    'Next_Level': "Prossimo Livello:",
+    'Messages': "Messaggi",
+    'Agora': "Agora",
+    'Treaty': "Accordi",
+    'Alliance': "Alleanza",
+    'Transport': "Ttrasporta Merci!",
+    'Send_General_Log': "Invia log all'alleanza",
+    'Send_General_Log_Header': "Attacchi contro i membri dell'alleanza",
+
+    'options': {
+        'transport': {
+            'header': 'Tipo di opzioni di Trasporto',
+            'original': 'GameForge',
+            'ikaeasy': 'IkaEasy'
+        },
+        'movement_tabs': {
+            'header': 'Movimento tab',
+            'enable': 'active',
+            'disable': 'unactive'
+        },
+        'gold_per_hour': {
+            'header': 'Produzione Oro',
+            'enable': 'active',
+            'disable': 'unactive'
+        },
+        'island_ap': {
+            'header': "Punti d'azione Isola",
+                'enable': 'active',
+    'disable': 'unactive'
+},
+'development': {
+    'overview': "IkaEasy è non commerciale, ma è un progetto <a href='https://github.com/swat-web/IkaEasy' target='_blank'>OpenSource</a>. Vuoi aiutare? Contribuisci su <a href='https://github.com/swat-web/IkaEasy' target='_blank'>GitHub</a>, con report riguardanti bug o suggerimenti, anche su <a href='https://www.facebook.com/ikariam.easy' target='_blank'>Facebook</a>.<br>Sviluppare IkariamEasy richiede molto tempo: week-end, serate e delle notti. Si può dire grazie, aiutandomi a rimanere sveglio con l'acquisto di me caffè :) <a href='http://ko-fi.com?i=13041DD5P60E0' target='_blank'><div class='img_center'><img style='border:0px' src='https://az743702.vo.msecnd.net/cdn/btn3.png' border='0' alt='Comprami un caffè su Ko-Fi.com' /></div></a> Passa a trovarci su <a href='https://chrome.google.com/webstore/detail/ikariam-easy-new-feature/nbgafacaepgodomleebpcjcfgeiceidb' target='_blank'>IkaEasy V2</a>, per tenerti aggiornato! <br> Special thanks to Luca Maglie for Italian translation"
+}
+},
+
+'ikalogs': {
+    'save_log': 'Salva log',
+        'analyze': 'Analizza',
+        'each': 'Ogni',
+        'help_bw': 'Separa i round individuali con una virgola oppure una serie di rounds con un trattino',
+
+        'get_info': 'Ottenere dati',
+        'get_round': 'Ottenere log',
+        'saving': 'Salvataggio log',
+
+        'saving_success': 'I log sono stati salvati con successo.',
+        'open_report': 'Apri log',
+
+        'saving_failed': 'Salvataggio log fallito',
+        'repeat': 'Prova ancora',
+
+        'auth': {
+        'not_logged': 'Non sei registrato su Ikalogs'
+    },
+
+    'types': {
+        'full': 'Log pieni',
+            'short': 'Log breve',
+            'last': 'Ultimo round',
+            'each': 'Ogni N round',
+            'between': 'Specifici round'
+    }
+},
+
+'Transporter': 'Trasportatore'
+};
