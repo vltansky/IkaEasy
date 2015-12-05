e66e04edd9f7a4dcbcb5ca512f76c6715a366ccc
Tansky <vl.tansky@gmail.com>
2015-12-05 20:13:46 +0200

    grunt

---
+++ Gruntfile.js
@@ -0,0 +1,82 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        cssmin: {
+            options: {
+                shorthandCompacting: false,
+                roundingPrecision: -1
+            },
+            target: {
+                files: {
+                    'dist/css/ikaeasy.css': [
+                        'master/css/ikaeasy.css',
+                        //'master/css/ikalogs.css'
+                    ]
+                }
+            }
+        },
+        uglify: {
+            options: {
+                mangle: false,
+                compress: {
+                    drop_console: true,
+                    hoist_funs: false
+                },
+                //sourceMap: true
+            },
+            my_target: {
+                files: {
+                    'dist/inner/ikaeasy.js': ['master/inner/ikaeasy.js'],
+                    'dist/inner/background.min.js': ['master/inner/background.js'],
+                    //'dist/jquery.min.js': ['master/jquery.min.js'],
+                    'dist/app.min.js': [
+                        'master/jquery.min.js',
+                        'master/langs/*.js',
+                        "master/zJS/utils.js",
+                        "master/zJS/lng.js",
+                        "master/zJS/var.js",
+                        "master/zJS/db.js",
+                        "master/init.js",
+                        "master/ikalogs.js",
+                        'master/json.js',
+                        'master/page/*.js',
+                        "master/hotkeysPlugin.js",
+                        "master/page/sendIKMessage.js",
+                        "master/zJS/navigation.js",
+                    ]
+                }
+            }
+        },
+        copy: {
+            main: {
+                files: [
+                    // includes files within path
+                    //{expand: true, src: ['master/icon/*'], dest: 'dist/icon/', filter: 'isFile'},
+
+                    // includes files within path and its sub-directories
+                    //{expand: true, src: ['master/icon/**'], dest: 'dist/icon/'},
+
+                    //// makes all src relative to cwd
+                    {expand: true, cwd: 'master/icon/', src: ['**'], dest: 'dist/icon/'},
+                    {expand: true, cwd: 'master/images/', src: ['**'], dest: 'dist/images/'},
+                    //
+                    //// flattens results to a single level
+                    //{expand: true, flatten: true, src: ['path/**'], dest: 'dest/', filter: 'isFile'},
+                ]
+            }
+        }
+        //watch: {
+        //    files: ['<%= jshint.files %>'],
+        //    tasks: ['jshint']
+        //}
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-postcss');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    //grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default', ['cssmin', 'uglify', 'copy']);
+
+};
---
+++ master/init.js
@@ -0,0 +1,71 @@
+﻿function appendScript(src) {
+    $('head').append('<script type="text/javascript" src="' + src + '"></script>');
+}
+
+function appendStyle(src) {
+    $('head').append('<link href="' + src + '"  rel="stylesheet" type="text/css" />');
+}
+
+function getiUrlImage(url) {
+    return chrome.extension.getURL('images/' + url);
+}
+
+function createDynamic(title, content) {
+    return zJS.Utils.createDynamic(title, content);
+}
+
+function getServerDomain() {
+    return zJS.Utils.getServerDomain();
+}
+
+function askms(params, handler) {
+    zJS.Utils.askms(params, handler || false);
+}
+
+function getJSON(url, callback) {
+    $.ajax({
+        url: url,
+        async: true,
+        dataType: "json",
+        error: callback,
+        success: callback
+    });
+}
+
+function postJSON(url, data, callback) {
+    $.ajax({
+        url: url,
+        data: data,
+        async: true,
+        type: "POST",
+        error: callback || false,
+        success: callback || false,
+        dataType: "json"
+    });
+}
+
+Array.prototype.removeEl = function() {
+    var what, a = arguments, L = a.length, ax;
+    while (L && this.length) {
+        what = a[--L];
+        while ((ax = this.indexOf(what)) !== -1) {
+            this.splice(ax, 1);
+        }
+    }
+    return this;
+};
+
+function containsObject(obj, list) {
+    var i;
+    for (i = 0; i < list.length; i++) {
+        if (list[i] === obj) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+appendStyle(zJS.Utils.getUrl('css/ikaeasy.css?version=2.0.0.91'));//@todo change with update
+//appendStyle(zJS.Utils.getUrl('css/ikalogs.css?version=2.0.0.91'));//@todo add if with settings
+appendStyle('/skin/compiled-' + zJS.Utils.getServerDomain() + '-island.css');
