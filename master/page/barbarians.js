c650c5915401fe9c106d8d2c157ad133504605be
MaLiN2223 <szyszka45@gmail.com>
2016-09-29 01:38:32 +0200

    Added barbarians and safehouse scripts. Barbarians : shows how much resources are going to be robbed. Safehouse : enhanced resources reports

---
+++ master/page/barbarians.js
@@ -0,0 +1,35 @@
+if(typeof zJS == "undefined") {
+    zJS = {};
+}
+
+if(typeof zJS.Page == "undefined") {
+    zJS.Page = {};
+}
+function getGoods(id) {
+    return parseInt($("[id=" + id + "]")[0].innerHTML.replace(',', ''));
+}
+function write() {
+    "use strict";
+    var resourcesBox = $('[class="barbarianCityInfosText"]');
+    if (resourcesBox.length == 0) {
+        return;
+    }
+    var wood = getGoods("js_islandBarbarianResourceresource");
+    var wine = getGoods("js_islandBarbarianResourcetradegood1");
+    var marble = getGoods("js_islandBarbarianResourcetradegood2");
+    var glass = getGoods("js_islandBarbarianResourcetradegood3");
+    var sulfur = getGoods("js_islandBarbarianResourcetradegood4");
+    var sum = wood + wine + marble + glass + sulfur;
+    var shipsCount = Math.ceil(sum / 500);
+
+    var placeForText = $("div[class=barbarianCityKingSpeech]");
+    placeForText.text("You will rob " + sum + " resources, you need " + shipsCount + " ships");
+}
+zJS.Page.barbarianVillage = {
+    init: function() {
+        write();
+    },
+    refresh: function() {
+        this.init();
+    }
+};
---
+++ master/page/safehouse.js
@@ -0,0 +1,84 @@
+if(typeof zJS == "undefined") {
+    zJS = {};
+}
+
+if(typeof zJS.Page == "undefined") {
+    zJS.Page = {};
+}
+function overrideResourcesReports() {
+    "use strict";
+    var resourcesTables = $("table[class*='reportTable resourcesTable']");
+    var i = 0 ;
+    for (i = 0; i < resourcesTables.length; ++i) {
+        var table = resourcesTables[i].children[0];
+        var tr = $("<tr/>");
+        tr.append($("<td/>", {text: "Warehouse level "}));
+        tr.append($("<td/>").append($("<input/>", {
+            style: "width:30px",
+            value: "0",
+            id: "warehouseLevel" + i,
+            class: "textfield"
+        }).on("change", refreshResourcesReports)));
+        tr.append($("<td/>", {id: "shipCount" + i}));
+        $(table).append(tr);
+        tr = $("<tr/>");
+        tr.append($("<td/>", {text: "Is idle?"}));
+        var checkbox = $("<input/>", {type: "checkbox", id: "isIdle" + i, class: "checkbox  radio image floatleft"});
+        checkbox.on("change", refreshResourcesReports);
+        tr.append($("<td/>").append(checkbox));
+        $(table).append(tr);
+        $(table.children[0]).append($("<th/>", {class: "count", text: "Available to steal "}));
+        for (var j = 1; j < table.children.length - 2; ++j) {
+            var row = $(table.children[j]);
+            row.append($("<td/>", {class: "count", text: "0"}));
+        }
+        refreshResourcesReports({target: checkbox[0]});
+    }
+}
+
+function calculateProtectedWithAllData(isActive, warehouseLevel){
+    "use strict";
+    const active = {
+        "warehouse": 480,
+        "townhall": 100
+    };
+    var current = { // inactive by default
+        "warehouse": 80,
+        "townhall": 15
+    };
+    if (isActive)
+        current = active;
+    return current.townhall + warehouseLevel * current.warehouse;
+}
+function refreshResourcesReports(e) {
+    var separator = zJS.Var.getSeparators()["thousand"];
+    console.log("refreshing ");
+    var box = e.target;
+    var number = box.id.substr(-1);
+    var table = box.parentNode.parentNode.parentNode;
+    var rows = $(table.children);
+    var isIdle = !$("#isIdle" + number).is(":checked");
+    var level = $("#warehouseLevel" + number).val();
+    var sum = 0;
+    console.log("level :"+level);
+    console.log("is idle "+isIdle);
+    var protectedResources = calculateProtectedWithAllData(isIdle, level);
+    for (var i = 1; i < rows.length - 2; ++i) {
+        var row = rows[i];
+        var value = parseInt(row.cells[1].innerText.replace(separator, ''));
+        console.dir(value);
+        var toSteal = value - protectedResources;
+        console.log("sum = "+sum);
+        sum += toSteal < 0 ? 0 : toSteal;
+        row.cells[2].innerText = toSteal < 0 ? 0 : toSteal;
+    }
+    $("#shipCount" + number).text("Sum : " + sum + " you will need " + Math.ceil(sum / 500) + " ships");
+}
+zJS.Page.safehouse = {
+    init: function() {
+        overrideResourcesReports();
+    },
+    refresh: function() {
+        this.init();
+    }
+};
